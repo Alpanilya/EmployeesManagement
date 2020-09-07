72b954f79065f3d9b6e298b20099d554081f3bb3
Alpanilya <alpanilya@gmail.com>
2020-09-07 12:34:54 +0700

    Базовый шаблон представления данных

--- EmployeesManagement/ViewModel/MainWindowViewModel.cs
+++ EmployeesManagement/ViewModel/MainWindowViewModel.cs
@@ -4,6 +4,11 @@ namespace EmployeesManagement.ViewModel
 {
     internal class MainWindowViewModel: BaseViewModel
     {
-        
+        private string _Title = "Employee Management";
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
     }
 }
