3cde76c4659a2080283c7318a19db50e86251954
Alpanilya <alpanilya@gmail.com>
2020-09-07 12:50:59 +0700

    Реализация модели

--- EmployeesManagement/Model/Departament.cs
+++ EmployeesManagement/Model/Departament.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using EmployeesManagement.Model.Interfaces;
 using System.Collections.Generic;
-using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EmployeesManagement.Model
 {
-    class Departament
+    internal class Departament: IEntity
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<Employee> Employees { get; set; }
     }
 }
--- EmployeesManagement/Model/Employee.cs
+++ EmployeesManagement/Model/Employee.cs
@@ -1,10 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using EmployeesManagement.Model.Interfaces;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EmployeesManagement.Model
 {
-    class Employee
+    internal class Employee: IEntity
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Position { get; set; }
+        public int Dep_Id { get; set; }
+        [ForeignKey(nameof(Dep_Id))]
+        public Departament Departament { get; set; }
     }
 }
